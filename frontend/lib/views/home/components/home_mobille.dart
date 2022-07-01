81fc1b36d76ee6e60d5f9005ceb41b2bcc87af22
anilson <anilsonmonteiro@gmail.com>
2022-07-01 07:32:51 -0100

    chore(frontend):clean

--- frontend/lib/views/home/components/home_mobille.dart
+++ frontend/lib/views/home/components/home_mobille.dart
@@ -17,6 +17,7 @@ import 'package:provider/provider.dart';
 
 class HomeMobile extends StatefulWidget {
   final HomeController controller;
+
   const HomeMobile(this.controller, {Key? key}) : super(key: key);
 
   @override
@@ -89,99 +90,7 @@ class _HomeMobileState extends State<HomeMobile> {
                                 ),
                               ),
                             ],
-                          )
-
-                          // child: GridView.builder(
-                          //   clipBehavior: Clip.none,
-                          //   shrinkWrap: true,
-                          //   padding: EdgeInsets.all(16),
-                          //   itemCount: productProvider.products.length,
-                          //   itemBuilder: (context, index) {
-                          //     Product product = productProvider.products[index];
-                          //     return Center(
-                          //       child: Container(
-                          //           color: Colors.redAccent,
-                          //           height: 300,
-                          //           width: double.maxFinite,
-                          //           child: Text("${product.name}")
-                          //           // ProductCard(
-                          //           //   action: () {},
-                          //           //   title: product.name,
-                          //           //   mainImage: '',
-                          //           //   price: "${product.price}",
-                          //           //   rating: '',
-                          //           // ),
-                          //           ),
-                          //     );
-                          //   },
-                          //   gridDelegate:
-                          //       SliverGridDelegateWithFixedCrossAxisCount(
-                          //           crossAxisCount: 2),
-                          // ),
-
-                          // child: ListView.builder(
-                          //   itemCount: provider.products.length,
-                          //
-                          //   // mainAxisAlignment: MainAxisAlignment.start,
-                          //   itemBuilder: (BuildContext context, int index) {
-                          //     Product product = productProvider.products[index];
-                          //
-                          //     return ProductCard(
-                          //       action: () {},
-                          //       title: product.name,
-                          //       mainImage: '',
-                          //       price: "${product.price}",
-                          //       rating: '',
-                          //     );
-                          //   },
-
-/*            children: [
-
-                          Container(
-                            padding: EdgeInsets.all(16),
-                            child: TextField(
-                              controller: textController,
-                            ),
-                          ),
-                          CategoryItems(),
-                          Container(
-                            height: 200,
-                            width: double.maxFinite,
-                            margin: EdgeInsets.all(16),
-                            decoration: BoxDecoration(
-                              color: AppColors.main,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: Center(child: Text("Hero slide")),
-                          ),
-                          Container(
-                            child: Text("New Trend"),
-                          ),
-                          Container(
-                            child: Expanded(
-                              child: Container(
-                                padding: EdgeInsets.all(16),
-                                child: GridView.builder(
-                                  itemCount: 4,
-                                  itemBuilder: (context, index) {
-                                    Product product = productList[index];
-                                    return ProductCard(
-                                      action: () {},
-                                      title: product.name,
-                                      mainImage: '',
-                                      price: "${product.price}",
-                                      rating: '',
-                                    );
-                                  },
-                                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 2),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],*/
-                          // ),
-                          ),
+                          )),
                     );
                   },
                 ),
@@ -211,31 +120,5 @@ class _HomeMobileState extends State<HomeMobile> {
     textController.value = TextEditingValue(text: "Search product");
 
     return SearchBarWidgetMobile(searchProvider: searchProvider);
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: TextField(
-        controller: textController,
-      ),
-    );
   }
-
-  // Widget buildProductList(ProductProvider productProvider) {
-  //   return GridView.count(
-  //     crossAxisCount: 2,
-  //     mainAxisSpacing: 16,
-  //     crossAxisSpacing: 16,
-  //     children: productProvider.products
-  //         .map(
-  //           (product) => ProductCard(
-  //             addToCartAction: () => Navigator.pushNamed(
-  //                 context, "${AppRoutes.productDetail}/${product.id}"),
-  //             title: product.name,
-  //             mainImage: product.gallery[0],
-  //             price: "${product.price}",
-  //             rating: '',
-  //           ),
-  //         )
-  //         .toList() as List<Widget>,
-  //   );
-  // }
 }
